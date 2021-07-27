57370b071ea8216b5a95096f6b3439c8d4310ca5
esthe <ko990727@hmail.com>
2021-07-27 11:50:59 +0900

    Completion MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project11_2/GalleryAdapter.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project11_2/GalleryAdapter.java
@@ -6,14 +6,21 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalleryAdapter extends BaseAdapter{
     Context context;
     int[] posterIds = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5,
             R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9,R.drawable.img10};
 
-    public GalleryAdapter(Context context){
+    int[] titleIds = {R.string.movie_name1,R.string.movie_name2,R.string.movie_name3,R.string.movie_name4,R.string.movie_name5,
+            R.string.movie_name6,R.string.movie_name7,R.string.movie_name8,R.string.movie_name9,R.string.movie_name10};
+
+    ImageView imgvLarge;
+    public GalleryAdapter(Context context, ImageView imgvLarge){
         this.context = context;
+        this.imgvLarge = imgvLarge;
     }
 
     @Override
@@ -37,6 +44,19 @@ public class GalleryAdapter extends BaseAdapter{
         imgv.setLayoutParams(new ViewGroup.LayoutParams(200,300));
         imgv.setImageResource(posterIds[i]);
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        final int pos = i;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgvLarge.setImageResource(posterIds[pos]);
+                Toast toast = new Toast(context);
+                View toastView = View.inflate(context, R.layout.toast1, null);
+                TextView textTitle = toastView.findViewById(R.id.text_title);
+                textTitle.setText(titleIds[pos]);
+                toast.setView(toastView);
+                toast.show();
+            }
+        });
         return imgv;
     }
 }
