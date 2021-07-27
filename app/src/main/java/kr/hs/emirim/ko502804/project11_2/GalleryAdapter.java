2c42cee1ee9a24191d45f763cb9a833af20e2c33
esthe <ko990727@hmail.com>
2021-07-27 11:12:14 +0900

    Edit MainActivity.java Commit

---
+++ app/src/main/java/kr/hs/emirim/ko502804/project11_2/GalleryAdapter.java
@@ -0,0 +1,42 @@
+package kr.hs.emirim.ko502804.project11_2;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class GalleryAdapter extends BaseAdapter{
+    Context context;
+    int[] posterIds = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5,
+            R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9,R.drawable.img10};
+
+    public GalleryAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return posterIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imgv = new ImageView(context);
+        imgv.setLayoutParams(new ViewGroup.LayoutParams(200,300));
+        imgv.setImageResource(posterIds[i]);
+        imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        return imgv;
+    }
+}
